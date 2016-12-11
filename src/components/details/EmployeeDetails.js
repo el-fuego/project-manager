800da0fea56cd59fa90be571aca6c0a0c3b19ce1
el-fuego <watt87@mail.ru>
2016-12-11 15:12:41 +0200

    add validation

--- src/components/details/EmployeeDetails.js
+++ src/components/details/EmployeeDetails.js
@@ -1,28 +1,33 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { partial } from 'lodash';
 import { Form, Button } from 'reactstrap';
 
 import EmployeeModel from '../../models/Employee';
 import EditableInput from '../EditableInput';
 import FormFieldWithError from '../FormFieldWithError';
+import EditableModel from '../decorators/EditableModel';
 
 
-export default class EmployeeDetails extends PureComponent {
+@EditableModel(EmployeeModel)
+export default class EmployeeDetails extends Component {
   static propTypes = {
     entity: PropTypes.instanceOf(EmployeeModel).isRequired,
-    onRemove: PropTypes.func
+    onRemove: PropTypes.func,
+    updateField: PropTypes.func.isRequired,
+    getFirstErrorFor: PropTypes.func.isRequired
   };
 
   render() {
-    const { entity } = this.props;
+    const { entity, getFirstErrorFor, updateField } = this.props;
 
     return (
       <Form>
-        <FormFieldWithError label="Name">
-          <EditableInput value={entity.name} />
+        <FormFieldWithError label="Name" error={getFirstErrorFor('name')} touched>
+          <EditableInput value={entity.name} onChange={partial(updateField, 'name')} />
         </FormFieldWithError>
 
-        <FormFieldWithError label="E-mail">
-          <EditableInput value={entity.email} />
+        <FormFieldWithError label="E-mail" error={getFirstErrorFor('email')} touched>
+          <EditableInput value={entity.email} onChange={partial(updateField, 'email')} />
         </FormFieldWithError>
 
         <Button color="link" onClick={this.props.onRemove}>
--- src/components/details/ProjectDetails.js
+++ src/components/details/ProjectDetails.js
@@ -1,37 +1,43 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { partial } from 'lodash';
 import { Form, Button } from 'reactstrap';
 
 import ProjectModel from '../../models/Project';
 import EditableInput from '../EditableInput';
 import EditableDateInput from '../EditableDateInput';
 import FormFieldWithError from '../FormFieldWithError';
+import EditableModel from '../decorators/EditableModel';
 
 
-export default class ProjectDetails extends PureComponent {
+@EditableModel(ProjectModel)
+export default class ProjectDetails extends Component {
   static propTypes = {
     entity: PropTypes.instanceOf(ProjectModel).isRequired,
-    onRemove: PropTypes.func
+    onRemove: PropTypes.func,
+    updateField: PropTypes.func.isRequired,
+    updateDateField: PropTypes.func.isRequired,
+    getFirstErrorFor: PropTypes.func.isRequired
   };
 
   render() {
-    const { entity } = this.props;
+    const { entity, getFirstErrorFor, updateField, updateDateField, onRemove } = this.props;
 
     return (
       <Form>
-        <FormFieldWithError label="Name">
-          <EditableInput value={entity.name} />
+        <FormFieldWithError label="Name" error={getFirstErrorFor('name')} touched>
+          <EditableInput value={entity.name} onChange={partial(updateField, 'name')} />
         </FormFieldWithError>
 
-        <FormFieldWithError label="Start Date">
-          <EditableDateInput value={entity.startDate} />
+        <FormFieldWithError label="Start Date" error={getFirstErrorFor('startDate')} touched>
+          <EditableDateInput value={entity.startDate} onChange={partial(updateDateField, 'startDate')} />
         </FormFieldWithError>
 
-        <FormFieldWithError label="End Date">
-          <EditableDateInput value={entity.endDate} />
+        <FormFieldWithError label="End Date" error={getFirstErrorFor('endDate')} touched>
+          <EditableDateInput value={entity.endDate} onChange={partial(updateDateField, 'endDate')} />
         </FormFieldWithError>
 
 
-        <Button color="link" onClick={this.props.onRemove}>
+        <Button color="link" onClick={onRemove}>
           Remove Project
         </Button>
       </Form>
