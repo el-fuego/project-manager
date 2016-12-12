36c4f6d8c0675d7a9bd0159b0642a54f7f659f3e
el-fuego <watt87@mail.ru>
2016-12-12 02:37:36 +0200

    add project employee validation

--- src/models/ProjectEmployee.js
+++ src/models/ProjectEmployee.js
@@ -1,4 +1,24 @@
-import { assignFields } from './_helpers';
+import { assignFields, getValidationErrors, DATE_VALIDATION_SCHEMA } from './_helpers';
+
+const VALIDATION_SCHEMA = {
+  projectId: {
+    presence: {
+      message: 'is required'
+    },
+    numericality: true
+  },
+  employeeId: {
+    presence: {
+      message: 'is required'
+    },
+    numericality: true
+  },
+  role: {
+    presence: true
+  },
+  startDate: DATE_VALIDATION_SCHEMA,
+  endDate: DATE_VALIDATION_SCHEMA
+};
 
 const FIELDS_NAMES = ['id', 'projectId', 'employeeId', 'startDate', 'endDate', 'role'];
 
@@ -9,5 +29,13 @@ export default class ProjectEmployee {
    */
   constructor(data) {
     assignFields(this, data, FIELDS_NAMES);
+  }
+
+  /**
+   * Validate current model data
+   * @return {Object} Validation errors keyed by field names
+   */
+  validate() {
+    return getValidationErrors(this, VALIDATION_SCHEMA);
   }
 }
